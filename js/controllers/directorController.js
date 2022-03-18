02dfd41ff90c949e4048f10664e0c8a802d94891
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2022-03-18 23:15:16 +0800

    refactored MVC style for Director

--- js/controllers/directorController.js
+++ js/controllers/directorController.js
@@ -1,1 +1,28 @@
 // eventlisteners, navigation, UI events
+
+import * as model from '../models/directorModel.js';
+import directorView from '../views/directorView.js';
+
+// const directorContainer = document.querySelector('.section-author');
+
+const controlDirector = async function() {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return
+
+    // LOADING DIRECTOR
+    await model.loadDirector(id);
+
+    // RENDERING DIRECTOR
+    directorView.render(model.state.director);
+
+
+
+  } catch (err) {
+    alert (err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlDirector));
--- js/views/directorView.js
+++ js/views/directorView.js
@@ -1,1 +1,48 @@
-directorView.js
+
+class DirectorView {
+  #parentElement = document.querySelector('.section-author');
+  #data;
+
+  render(data) {
+    this.#data = data;
+    const markup = this.#generateMarkup();
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  #generateMarkup() {
+    return `
+      <div class="section-author">
+        <div class="section-author-photo">
+          <img class="author-photo" src="img/authors/hu-jie.jpg" alt="">
+        </div>
+        <div class="section-author-text">
+          <h1>${this.#data.name}</h1>
+          <div class="section-author-bio">
+            <div class="section-author-bio-short">
+              <p>${this.#data.bioShort} <ion-icon class="plus-icon" name="add-circle-outline"></ion-icon>
+              </p>
+            </div>
+            <div class="section-author-bio-long">
+              <p>${this.#data.bioLong} <span>Source: Wikipedia</span>
+              </p>
+            </div>
+          </div>
+          </div>
+        <div class="section-author-filmo">
+          <h3 class="section-author-filmo-title">Filmography</h3>
+          <div class="section-author-filmo-list">
+            <ul>${this.#data.docs.map(doc => {
+              return `<li><a href="documentary.html">${doc.name} (${doc.year})</a></li>`
+            }).join('')}
+            </ul>
+          </div>
+        </div>`;
+  }
+
+  #clear() {
+    this.#parentElement.innerHTML = '';
+  }
+};
+
+export default new DirectorView();
