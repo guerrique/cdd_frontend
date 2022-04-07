64a65bb716cb0da2f7c555acacd60271466873ad
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2022-04-07 15:50:55 +0800

    creation of searchModel.js

--- js/controllers/searchController.js
+++ js/controllers/searchController.js
@@ -1,13 +1,21 @@
 import searchView from '../views/searchView.js';
+import * as model from '../models/searchModel.js';
 
 const controlSearch = async function() {
   try {
+
+    // Get search query
     const query = searchView.getQuery();
     if(!query) return;
-    console.log(query);
+    // console.log(query);
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
 
   } catch(err) {
-    console.log(err);
+    console.log('error from the controller: ',err);
   }
 }
 
